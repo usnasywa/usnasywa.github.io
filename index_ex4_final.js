6683870b8464262178c806a1863535cc726a1093
usnasywa <nasywalabibah30@gmail.com>
2024-11-26 22:10:53 +0700

    Add files via upload

---
+++ index_ex4_final.js
@@ -0,0 +1,163 @@
+import * as THREE from './modules/three.module.js';
+
+main();
+
+function main() {
+    // create context
+    const canvas = document.querySelector("#c");
+    const gl = new THREE.WebGLRenderer({
+        canvas,
+        antialias: true
+    });
+
+    // create camera
+    const angleOfView = 55;
+    const aspectRatio = canvas.clientWidth / canvas.clientHeight;
+    const nearPlane = 0.1;
+    const farPlane = 100;
+    const camera = new THREE.PerspectiveCamera(
+        angleOfView,
+        aspectRatio,
+        nearPlane,
+        farPlane
+    );
+    camera.position.set(0, 8, 30);
+
+    // create the scene
+    const scene = new THREE.Scene();
+    // scene.background = new THREE.Color(0.3, 0.5, 0.8);
+    const fog = new THREE.Fog("grey", 1,90);
+    scene.fog = fog;
+
+    // GEOMETRY
+    // create the cube
+    const cubeSize = 5;
+    const cubeGeometry = new THREE.BoxGeometry(
+        cubeSize,
+        cubeSize,
+        cubeSize
+    );  
+
+    // Create the Sphere
+    const sphereRadius = 5;
+    const sphereWidthSegments = 32;
+    const sphereHeightSegments = 16;
+    const sphereGeometry = new THREE.SphereGeometry(
+        sphereRadius,
+        sphereWidthSegments,
+        sphereHeightSegments
+    );
+
+    // Create the upright plane
+    const planeWidth = 256;
+    const planeHeight =  128;
+    const planeGeometry = new THREE.PlaneGeometry(
+        planeWidth,
+        planeHeight
+    );
+
+    // MATERIALS
+    const textureLoader = new THREE.TextureLoader();
+    scene.background = new textureLoader.load('textures/night sky.jpg');
+
+    // const cubeMaterial = new THREE.MeshPhongMaterial({
+    //     color: 'pink'
+    // });
+
+    const cubeTexturemap = textureLoader.load('textures/gray_rocks_diff_2k.jpg');
+    const cubeMaterial = new THREE.MeshStandardMaterial({
+        map: cubeTexturemap
+    });
+
+    const sphereNormalMap = textureLoader.load('textures/rusty_metal_grid_diff_2k.jpg');
+    sphereNormalMap.wrapS = THREE.RepeatWrapping;
+    sphereNormalMap.wrapT = THREE.RepeatWrapping;
+    sphereNormalMap.repeat.set(4,4);
+    const sphereMaterial = new THREE.MeshStandardMaterial({
+        // color: 'purple',
+        map: sphereNormalMap
+    });
+
+    const planeTextureMap = textureLoader.load('textures/brick_wall_08_diff_2k.jpg');
+    planeTextureMap.wrapS = THREE.RepeatWrapping;
+    planeTextureMap.wrapT = THREE.RepeatWrapping;
+    planeTextureMap.repeat.set(10, 10);
+    //planeTextureMap.magFilter = THREE.NearestFilter;
+    planeTextureMap.minFilter = THREE.NearestFilter;
+    planeTextureMap.anisotropy = gl.getMaxAnisotropy();
+    const planeNorm = textureLoader.load('textures/brick_wall_08_nor_gl_2k.png');
+    planeNorm.wrapS = THREE.RepeatWrapping;
+    planeNorm.wrapT = THREE.RepeatWrapping;
+    planeNorm.minFilter = THREE.NearestFilter;
+    planeNorm.repeat.set(10, 10);
+    const planeMaterial = new THREE.MeshStandardMaterial({
+        map: planeTextureMap,
+        side: THREE.DoubleSide,
+        normalMap: planeNorm 
+    });
+
+    // MESHES
+    const cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
+    cube.position.set(cubeSize + 1, cubeSize + 1, 0);
+    scene.add(cube);
+
+    const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
+    sphere.position.set(-sphereRadius - 1, sphereRadius + 2, 0);
+    scene.add(sphere);
+
+    const plane = new THREE.Mesh(planeGeometry, planeMaterial);
+    plane.rotation.x = Math.PI / 2;
+    //scene.add(plane);
+
+    //LIGHTS
+    const color = 0xffffff;
+    const intensity = .9;
+    const light = new THREE.DirectionalLight(color, intensity);
+    light.target = plane;
+    light.position.set(0, 30, 30);
+    scene.add(light);
+    scene.add(light.target);
+
+    const ambientColor = 0xffffff;
+    const ambientIntensity = 0.5;
+    const ambientLight = new THREE.AmbientLight(ambientColor, ambientIntensity);
+    scene.add(ambientLight);
+
+    // DRAW
+    function draw(time){
+        time *= 0.001;
+
+        if (resizeGLToDisplaySize(gl)) {
+            const canvas = gl.domElement;
+            camera.aspect = canvas.clientWidth / canvas.clientHeight;
+            camera.updateProjectionMatrix();
+        }
+        
+        cube.rotation.x += 0.01;
+        cube.rotation.y += 0.01;
+        cube.rotation.z += 0.01;
+
+        // sphere.rotation.x += 0.01;
+        // sphere.rotation.y += 0.01;
+        sphere.rotation.y += 0.01;
+
+        // light.position.x = 20*Math.cos(time);
+        // light.position.y = 20*Math.sin(time);
+        gl.render(scene, camera);
+        requestAnimationFrame(draw);
+    }
+
+    requestAnimationFrame(draw);
+}
+
+// UPDATE RESIZE
+function resizeGLToDisplaySize(gl) {
+    const canvas = gl.domElement;
+    const width = canvas.clientWidth;
+    const height = canvas.clientHeight;
+    const needResize = canvas.width != width || canvas.height != height;
+    if (needResize) {
+        gl.setSize(width, height, false);
+    }
+    return needResize;
+}
